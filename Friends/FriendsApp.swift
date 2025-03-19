377ae5f62bd76029bbac361451abcd286f6c3a35
bazimR <rishaalmuhammed@gmail.com>
2025-03-20 01:15:29 +0530

    Initial Commit

---
+++ Friends/FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsApp.swift
+//  Friends
+//
+//  Created by Rishal Bazim on 20/03/25.
+//
+
+import SwiftUI
+
+@main
+struct FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FriendsTests/FriendsTests.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsTests.swift
+//  FriendsTests
+//
+//  Created by Rishal Bazim on 20/03/25.
+//
+
+import Testing
+@testable import Friends
+
+struct FriendsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
